7c4927ef457262e4c3a9ad6ce1fe20be32ff4ab7
JbsbhsKsuwabe <152618638+JbsbhsKsuwabe@users.noreply.github.com>
2024-02-23 10:36:08 -0300

    Update LP1Atividade4.java

--- JavaApplication1/src/AtividadeLPI/LP1Atividade4.java
+++ JavaApplication1/src/AtividadeLPI/LP1Atividade4.java
@@ -3,13 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package AtividadeLPI;
+
 import javax.swing.JOptionPane;
+
 /**
  *
  * @author joabe
  */
 public class LP1Atividade4 {
-public static void main(String args[]){
-    
+
+    public static void main(String args[]) {
+
+        int i, k = 0;
+
+        i = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor"));
+        while (i > 0) {
+            i = i / 10;
+            k++;
+        }
+        JOptionPane.showMessageDialog(null, "Contem " + k + " digitos.");
+
     }
 }
